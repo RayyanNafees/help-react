64e6bf9947734e66506105866e322d32fe51595c
RayyanNafees <nafees.rayyan@gmail.com>
2022-10-07 21:12:24 +0530

    Initial commit

--- src/Login.js
+++ src/Login.js
@@ -1,40 +1,66 @@
-import { Button } from '@mui/material'
-import React from 'react'
-import "./Login.css";
+import { Button } from '@mui/material';
+import React from 'react';
+import './Login.css';
 import { auth, provider } from './firebase';
 import { actionTypes } from './reducer';
 import { useStateValue } from './StateProvider';
-
+import { signIn as signin } from './axios';
 function Login() {
-    const [state, dispatch] = useStateValue();
+  const [state, dispatch] = useStateValue();
+  const [name, setName] = React.useState(''),
+    [password, setPassword] = React.useState('');
 
-    const signIn = () => {
-        auth.signInWithPopup(provider)
-            .then(result => {
-                dispatch({
-                    type: actionTypes.SET_USER,
-                    user: result.user,
-                })
-                console.log(result)
-            })
-            .catch((error) => alert(error.message));
-    };
-    return (
-        <div className='login'>
-            <div className='login__logo'>
-                <img src="https://img01.techsoft.co.uk/o/ts/LayoutImages/FacebookLogo.png" alt="" />
+  const signIn = React.useCallback(
+    () =>
+      signin({
+        username: name,
+        password,
+        profilePic: 'profile',
+        email: name.includes('@') ? name : name.toLowerCase() + '@gmail.com',
+      })
+        .then((result) => {
+          dispatch({
+            type: actionTypes.SET_USER,
+            user: result.user,
+          });
+          console.log(result);
+        })
+        .catch((error) => alert(error.message)),
+    [name, password, dispatch]
+  );
+  return (
+    <div className='login'>
+      <div className='login__logo'>
+        <img
+          src='https://img01.techsoft.co.uk/o/ts/LayoutImages/FacebookLogo.png'
+          alt=''
+        />
 
-
-                <img style={{ height: "50px", marginTop: "50px" }} src="https://th.bing.com/th/id/OIP.v3L3OxHd_Wq671UhFRq9nAHaB_?w=305&h=93&c=7&r=0&o=5&dpr=1.25&pid=1.7" alt=" " />
-            </div>
-            <form>
-                <input type="text" placeholder='username' />
-                <input type="password" placeholder='password' />
-            </form>
-            <Button type="submit" onClick={signIn}> Sign In </Button>
-        </div>
-
-    )
+        <img
+          style={{ height: '50px', marginTop: '50px' }}
+          src='https://th.bing.com/th/id/OIP.v3L3OxHd_Wq671UhFRq9nAHaB_?w=305&h=93&c=7&r=0&o=5&dpr=1.25&pid=1.7'
+          alt=' '
+        />
+      </div>
+      <form>
+        <input
+          type='text'
+          placeholder='username'
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <input
+          type='password'
+          placeholder='password'
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </form>
+      <Button type='submit' onClick={signIn}>
+        Sign In
+      </Button>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,9 @@
-import axios from "axios";
+import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: "http://127.0.0.1:5000"
-})
+  baseURL: 'http://localhost:5000/api/v1',
+});
 
-export default instance
+export default instance;
+
+export const signIn = (data) => instance.post('/auth/register', data);
